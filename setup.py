ac30ced6af66566462e57a524f78b414e8ba3c9c
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-12-21 09:51:26 -0800

    feat: Initialize project structure with setup, logging, and initial LLM/embedding experimentation.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="document_RAG",
+    author="Harsh Saw",
+    author_email="mister.harshkumar@gmail.com",
+    version="0.1",
+    packages=find_packages(),
+
+)
