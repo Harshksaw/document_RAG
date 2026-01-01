b81d1981349ae447c16dfe7499d6e2541ffc8792
Harshksaw <mister.harshkumar@gmail.com>
2026-01-01 19:30:28 +0000

    feat: Add Metadata model and enhance DocumentAnalyzer initialization with logging and error handling

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,15 +1,35 @@
 import os
 from utils.model_loader import ModelLoader
-from models.models import *
+from models.model import Metadata
 from exception.custom_exception import DocumentPortalException
 from langchain.output_parsers import OutputFixingParser
 from langchain_core.output_parsers import JsonOutputParser
-
+from logger.custom_logger import CustomLogger
+from prompt.prompt_library import *
 
 class DocumentAnalyzer:
     
     def __init__(self):
-        pass
+        self.log = CustomLogger().get_logger(__name__)
+        
+        try:
+            self.loader = ModelLoader()
+            self.llm = self.loader.load_llm()
+            
+            #prepare parser
+            self.parser = JsonOutputParser(pydanctic_obeject=Metadata)
+            self.fixing_parser - OutputFixingParser.from_llm(self.llm, self.parser)
+            
+            self.prompt = PROMPT_REGISTRY["document_analysis"]
+            
+            self.log.info("DocumentAnalyzer initialized successfully")
+            
+            
+            
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentAnalyzer: {e}")
+            raise DocumentPortalException("Error initializing DocumentAnalyzer", e) from e
+        
     
     def analyze_metadata(self):
         pass
