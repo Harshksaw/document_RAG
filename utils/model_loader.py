ff4f5634986847e943a692ba101f18b5b7f43d57
Harshksaw <mister.harshkumar@gmail.com>
2026-01-01 18:27:34 +0000

    feat: Add document handling capabilities with PDF saving and reading, and implement custom exception handling

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
 from logger import GLOBAL_LOGGER as log
-from exception.custom_exeception import DocumentPortalException
+from exception.custom_exception import DocumentPortalException
 
 
 class ApiKeyManager:
