cdf571a95adbba24d9ee8d03e7bf74e01b370ae5
Harshksaw <mister.harshkumar@gmail.com>
2026-01-01 19:57:19 +0000

    feat: Enhance Metadata model and implement DocumentAnalyzer with improved error handling and document analysis capabilities

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,6 +1,7 @@
-import os
+
+import sys
 from utils.model_loader import ModelLoader
-from models.model import Metadata
+from models.model import *
 from exception.custom_exception import DocumentPortalException
 from langchain.output_parsers import OutputFixingParser
 from langchain_core.output_parsers import JsonOutputParser
@@ -17,8 +18,8 @@ class DocumentAnalyzer:
             self.llm = self.loader.load_llm()
             
             #prepare parser
-            self.parser = JsonOutputParser(pydanctic_obeject=Metadata)
-            self.fixing_parser - OutputFixingParser.from_llm(self.llm, self.parser)
+            self.parser = JsonOutputParser(pydantic_object=Metadata)
+            self.fixing_parser = OutputFixingParser.from_llm(self.llm, self.parser)
             
             self.prompt = PROMPT_REGISTRY["document_analysis"]
             
@@ -28,8 +29,26 @@ class DocumentAnalyzer:
             
         except Exception as e:
             self.log.error(f"Error initializing DocumentAnalyzer: {e}")
-            raise DocumentPortalException("Error initializing DocumentAnalyzer", e) from e
+            raise DocumentPortalException("Error initializing DocumentAnalyzer", sys) from e
         
     
-    def analyze_metadata(self):
-        pass
+    def analyze_document(self, document_text:str)->dict:
+        
+        try:
+            chain= self.prompt | self.llm | self.fixing_parser
+            
+            self.log.info("Metadata  analysis chain intialized")
+            
+            
+            response = chain.invoke({
+                "format_instructions": self.parser.get_format_instructions(),
+                "document_text": document_text
+            })
+            
+            self.log.info("Document analyzed successfully", keys=list(response.keys()   ))
+            return response 
+            
+        except Exception as e:
+            self.log.error(f"Error analyzing document: {e}")
+            raise DocumentPortalException("Error analyzing document", sys) from e
+            raise DocumentPortalException("Error analyzing document", e) from e
