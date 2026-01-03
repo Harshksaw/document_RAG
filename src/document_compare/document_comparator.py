e6c0eb56bde67f1589915a967c1b28aa01715236
Harshksaw <mister.harshkumar@gmail.com>
2026-01-03 05:26:05 +0000

    feat: Initialize DocumentComparator with LLM, output parsers, and chain, and rename placeholder methods.

--- src/document_compare/document_comparator.py
+++ src/document_compare/document_comparator.py
@@ -4,19 +4,35 @@ import fitz
 
 from logger.custom_logger import CustomLogger
 from exceptions.custom_exception import CustomException 
+from prompt.prompt_library import PROMPT_REGISTRY
+from utils.model_loader import ModelLoader
+from langchain_core.outpi
 
 class DocumentComparator:
     def __init__(self):
-        pass
-    def delete_exisiting_files(self, path: Path):
+        load_dotenv()
+        self.log = CustomLogger()
+        self.loader = ModelLoader()
+        self.llm = self.loader.load_model()
+        self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
+        self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
+        self.prompt = PROMPT_REGISTRY[PromptType.DOCUMENT_COMPARISON]
+        self.chain =self.prompt | self.llm | self.parser| self.fixing_parser
+        self.log.info("DocumentComparator initialized successfully")    
+
+
+
+
+
+    def compare_documents(self):
         try:
-            if path.exists():
-                for file in path.iterdir():
-                    file.unlink()
+            pass
+            
         except Exception as e:
+            self.logger.error("Error in compare_documents method of DocumentComparator class", e)
             raise CustomException(e, sys) from e
     
-    def save_uploaded_files(self):
+    def format_reponse(self):
         pass
     
     def read_pdf(self):
