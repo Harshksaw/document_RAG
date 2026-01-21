e12b258730522b658157f2673d386e60540fb55a
Harshksaw <mister.harshkumar@gmail.com>
2026-01-21 05:49:16 +0000

    feat: Introduce core components for multi-document RAG, including conversational retrieval and document ingestion.

---
+++ src/multi_document_chat/data_ingestion.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+class DocumentIngestor:
+    def __init__(self, documents: List[str]):
+        pass
+
+    def ingest_files(self):
+        pass
+
+    def _create_retriever(self, documents: List[str]):
+        pass
---
+++ src/multi_document_chat/retrieval.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+class ConversationalRAG():
+
+    def __init__(self, retriever: BaseRetriever):
+        pass
+
+    def load_retriever_from_faiss(self):
+        pass
+
+    class invoke(self):
+        pass
+
+    class _load_llm(self):
+        pass
+
+
+    @staticmethod
+    def _format_docs(docs):
+        pass
+
+
+    def _build_lcel_chain(self):
+        pass
+    
