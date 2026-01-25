86c5c2b3c989cf420bd56d84f9e48d24560b40c1
Harshksaw <mister.harshkumar@gmail.com>
2026-01-25 02:12:14 +0000

    feat: Add diverse document samples and update data ingestion for multi-document chat.

--- src/multi_document_chat/data_ingestion.py
+++ src/multi_document_chat/data_ingestion.py
@@ -47,6 +47,46 @@ class DocumentIngestor:
 
     def ingest_files(self):
         try:
+
+            documents =[]
+            for uploaded_file in self.uploaded_files:
+                    ext = Path(uploaded_file.name).suffix.lower()
+                    if ext not in self.SUPPORTED_EXTENSIONS:
+                        self.log.warning("Unsupported file type:", filename= uploaded_file.name)
+                        continue
+
+                    unique_filename = f"{uuid.uuid4().hex[:8]}{ext}"
+                    temp_path = self.session_temp_dir / unique_filename
+
+                    with open(temp_path, "wb") as f:
+                        f.write(uploaded_file.read())
+                    self.log.info("File saved to:", session_id = self.session_id, filename = uploaded_file.name, saved_as=str(temp_path))
+
+                    if ext == ".pdf":
+                        loader = PyPDFLoader(str(temp_path))
+                    elif ext == ".docx":
+                        loader = Docx2txtLoader(str(temp_path))
+                    elif ext == ".txt":
+                        loader = TextLoader(str(temp_path))
+                    elif ext == ".md":
+                        loader = TextLoader(str(temp_path))
+                    else:
+                        self.log.warning("Unsupported file type:", filename= uploaded_file.name)
+                        continue
+                    
+                    docs = loader.load()
+                    documents.extend(docs)
+
+                    if not documents:
+                        self.log.warning("No documents loaded")
+                        raise DocumentPortalException("No documents loaded")
+
+
+                    self.log.info("Documents loaded successfully", session_id = self.session_id, num_docs = len(documents))
+                    return documents
+
+                    
+                    
             
 
         except Exception as e:
